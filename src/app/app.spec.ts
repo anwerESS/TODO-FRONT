70b7b2790a9313dc6d124c3d07d4095f0ccaa185
anweress <chlayfa.anouar@gmail.com>
2026-05-13 19:55:15 +0200

    clean & prepare project

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TODO-FRONT');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Todo App');
   });
 });
